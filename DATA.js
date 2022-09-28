c0befb619876f1d54dd0e7e0c4425bc5434c5c87
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 18:11:02 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "ba21046b-d63b-461b-8664-5969e089e380",
+    "home_feed": [
+        {
+            "id": "5ed6aa04-f776-4d8c-9880-98d8eae5209d",
+            "artist_name": "child",
+            "home_carousel": [
+                {
+                    "id": "5ecf1e68-cba5-43ba-b6a8-43a67afc0f10",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/141.png"
+                },
+                {
+                    "id": "e485e7b1-d628-4c59-8250-17d5835c4bea",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/146.png"
+                },
+                {
+                    "id": "e207e291-a989-42ea-92ca-8d520d52da1e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/287.png"
+                },
+                {
+                    "id": "62664709-7390-488b-af6d-bb5008452d03",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/122.png"
+                },
+                {
+                    "id": "ebe39920-c04e-4d60-95e0-efc12fb84145",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/302.png"
+                }
+            ],
+            "art_caption": "toward",
+            "art_description": "Significant painting board whose. Early one TV health can edge. Among even production recently morning husband wonder she."
+        },
+        {
+            "id": "701bfed7-d026-4a2c-9e2b-1875daaf82c2",
+            "artist_name": "movement",
+            "home_carousel": [
+                {
+                    "id": "481c6029-7ab0-4fb1-887c-e55fc3f5b947",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/262.png"
+                },
+                {
+                    "id": "c2c9ee00-c9ac-4833-8c4e-9a1144963471",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png"
+                },
+                {
+                    "id": "f0aadb89-9709-4a0a-a0d2-ffc4eeb8872b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/103.png"
+                },
+                {
+                    "id": "4dbe73b1-9499-4fde-8eb8-86f3710e167e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/230.png"
+                },
+                {
+                    "id": "a9614eea-a5ef-4be8-ba3d-114cfd4d8e0e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/193.png"
+                }
+            ],
+            "art_caption": "society",
+            "art_description": "Me do central matter. Moment court service glass decision."
+        },
+        {
+            "id": "516e7f1c-7117-4989-bec4-7ed888988d12",
+            "artist_name": "my",
+            "home_carousel": [
+                {
+                    "id": "c09a6012-ce56-4053-a5bf-4b9fcccb9e14",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "79c84876-dbc5-472c-bc3c-0e442db25822",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/174.png"
+                },
+                {
+                    "id": "92d88160-3796-40dc-b5b7-284fe7b41f55",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png"
+                },
+                {
+                    "id": "178c32de-89f8-4a23-a3f8-439cb0904e33",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/72.png"
+                },
+                {
+                    "id": "bf2ee770-e106-4ebb-ba38-30be7c78f026",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/112.png"
+                }
+            ],
+            "art_caption": "effect",
+            "art_description": "Four recent a. Point painting himself image political everybody place."
+        },
+        {
+            "id": "7ecdabe8-1d23-48b4-b71d-d94988d6f3b0",
+            "artist_name": "record",
+            "home_carousel": [
+                {
+                    "id": "035a897a-d777-4c42-932e-c74748287341",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/37.png"
+                },
+                {
+                    "id": "1656e567-8c0e-4612-911b-a4126ab6591c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+                },
+                {
+                    "id": "54764116-0ca3-43bd-974c-24ec136016ae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/327.png"
+                },
+                {
+                    "id": "584680a8-0def-4346-a931-9baba825561a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                },
+                {
+                    "id": "74764b86-0c38-43e5-ac66-71cd30258be7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png"
+                }
+            ],
+            "art_caption": "wait",
+            "art_description": "Least should quite concern beautiful prove key. Woman common material voice traditional use the. Gun scene share lawyer water health."
+        },
+        {
+            "id": "e32b6b01-63e3-4c22-bfe8-4b5560e79b50",
+            "artist_name": "capital",
+            "home_carousel": [
+                {
+                    "id": "53c8bdd7-3a28-4c2b-a8b7-ad1880dbdfd4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/217.png"
+                },
+                {
+                    "id": "7661f6f4-ed28-487b-b5a4-52457bffda6b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/118.png"
+                },
+                {
+                    "id": "0067cbbc-8c62-4641-9f48-4b64d100cdb6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/243.png"
+                },
+                {
+                    "id": "98e85550-9357-47e8-9cf7-cd7df0dfb722",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/288.png"
+                },
+                {
+                    "id": "9a9091fb-8168-45f0-9ca1-8e637de75801",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/341.png"
+                }
+            ],
+            "art_caption": "a",
+            "art_description": "Our model magazine type. Night pressure some by capital article big."
+        }
+    ]
+}
